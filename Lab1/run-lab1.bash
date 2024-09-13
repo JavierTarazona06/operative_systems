ead1846eed6154bc119d2823256f6e0c94a1cd5e
Javier Andres Tarazona Jimenez <elr490@usask.ca>
2024-09-12 18:53:59 -0600

    Bash creation: get sys.. archic, set binary depending on it, assuring format and call binary

---
+++ Lab1/run-lab1.bash
@@ -0,0 +1,50 @@
+/* NAME: Javier Andres Tarazona Jimenez, Nahian Sajid 
+   NSID: elr490, Zas742
+   Student Number: 11411898, 11309563
+
+   CMPT 332 Term 1 2024
+
+   Lab 1
+
+   September 12, 2024
+   Modified September 12, 2024
+*/
+
+#!/bin/bash
+
+SYSARCH = ${uname -m}
+
+case $SYSARCH in
+	x86_64)
+		ARCHIC = "x86_64"
+		BINA = "sample_linux"
+		;;
+	aarch64 | arm64 | arm)
+		ARCHIC = "arm"
+		BINA = "sample_linux-arm"
+		;;
+	ppc64 | ppc)
+		ARCHIC = "ppc"
+		BINA = "sample_linux-ppc"
+		;;
+	*)
+		eco "Unsupported achitecture: $SYSARCH"
+		exit 1
+		;;
+esac
+
+if [ "$#" -lt 1 ] || [ "$#" -gt 1 ]; then
+	echo "An argument must be sent"
+	exit 1
+fi
+
+if [ ! -f "$BINA" ]; then
+	echo "Oh no, there is no file $BINA :("
+	exit 1
+fi
+
+MYARGS = "$@"
+
+$BINA $MYARGS
+
+
